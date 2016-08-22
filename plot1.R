2a403101e747955658475266a2e88cc254f66f39
jspaz <paz.sergio@gmail.com>
2016-08-22 10:11:06 -0500

    course project 1

--- plot1.R
+++ plot1.R
@@ -1,1 +1,5 @@
-# 
+# Plot 1
+hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+# Create a png file and close
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
--- plot2.R
+++ plot2.R
@@ -1,1 +1,5 @@
-# 
+# Plot 2
+plot(t$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+# Create a png file and close
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
